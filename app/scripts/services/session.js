8fc2d0829177ecf257f539281965b4d7427c88fd
Stefan Valentin <khalilravanna@gmail.com>
2014-09-13 12:23:00 -0500

    Fixed awful 2 space indentation

--- app/scripts/services/session.js
+++ app/scripts/services/session.js
@@ -1,6 +1,6 @@
 'use strict';
 
 angular.module('findieApp')
-  .factory('Session', function ($resource) {
-    return $resource('/api/session/');
-  });
+.factory('Session', function ($resource) {
+	return $resource('/api/session/');
+});
