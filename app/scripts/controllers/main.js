d24d9f31c1c0f245587b8db11f729ccc0c202621
Stefan Valentin <khalilravanna@gmail.com>
2015-01-17 18:15:17 -0600

    Basic landing page redesign with tabs and header

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,7 +2,5 @@
 
 angular.module('findieApp')
 .controller('MainCtrl', function ($scope, $http) {
-	$http.get('/api/awesomeThings').success(function(awesomeThings) {
-		$scope.awesomeThings = awesomeThings;
-	});
+	$scope.tab = 1;
 });
