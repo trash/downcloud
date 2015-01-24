a2b02ffb9513c466b166bc9f99d7868b774c0ed2
Stefan Valentin <khalilravanna@gmail.com>
2015-01-23 19:52:21 -0600

    Got basic functionining create artwork route for api
    Updated some express code to newer api

--- app/scripts/controllers/art-sell.js
+++ app/scripts/controllers/art-sell.js
@@ -1,10 +1,19 @@
 'use strict';
 
 angular.module('findieApp')
-.controller('ArtSellCtrl', ['$scope', function ($scope) {
+.controller('ArtSellCtrl', ['$scope', '$http', function ($scope, $http) {
 	$scope.artwork = {};
 
-	$scope.submitArt = function () {
-		console.log('lol sup');
+	$scope.submitArt = function (form) {
+		// should really be using the ampersand model and calling save
+		$http({
+			url: '/api/art',
+			method: 'POST',
+			data: $scope.artwork
+		}).then(function () {
+			console.log('success', arguments);
+		}, function () {
+			console.log('error', arguments);
+		});
 	};
 }]);
